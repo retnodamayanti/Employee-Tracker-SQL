5bf5f92782a5529b860f1fa404cdbdef898a700b
retnodamayanti <mayahere5@gmail.com>
2023-06-05 13:37:47 +0930

    edited decimal in salary schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(13,2) NOT NULL,
+    salary DECIMAL(6,2) NOT NULL,
     department_id INT NOT NULL, 
     FOREIGN KEY (department_id)
     REFERENCES department(id)
