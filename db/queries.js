c802648dd6cc561ce11df4978d1d956f63b96414
retnodamayanti <mayahere5@gmail.com>
2023-06-08 15:43:36 +0930

    addedquery for view all employees

--- db/queries.js
+++ db/queries.js
@@ -19,7 +19,7 @@ function DatabaseQueries(promptCallback) {
   };
 
   this.viewAllEmployees = function () {
-    database.query('SELECT * FROM employees', function (err, results) {
+    database.query('SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.department_name AS department, roles.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id LEFT JOIN employees AS manager ON employees.manager_id = manager.id', function (err, results) {
       if (err) throw err;
       console.table(results);
       promptCallback();
