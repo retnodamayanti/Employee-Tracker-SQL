2c6a3a94e0f036ff70d3a1e26fad302bfa0bf705
retnodamayanti <mayahere5@gmail.com>
2023-06-08 14:51:47 +0930

    edited table name

--- db/schema.sql
+++ db/schema.sql
@@ -4,33 +4,31 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 -- create department table
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
 -- create role table
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(6,2) NOT NULL,
-    department_id INT NOT NULL, 
+    salary DECIMAL(13,2) NOT NULL,
+    department_id INT, 
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    REFERENCES departments(id)
     ON DELETE SET NULL
 );
 
 -- create employee table
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
     ON DELETE SET NULL
     
 );
