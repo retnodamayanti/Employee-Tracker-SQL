8394875bd2b1bbacd0b0ce7698b8f7a5f7eacca9
retnodamayanti <mayahere5@gmail.com>
2023-06-08 14:52:34 +0930

    added queries.js file

---
+++ db/queries.js
@@ -0,0 +1,41 @@
+const database = require('./db');
+const consoleTable = require('console.table');
+
+function DatabaseQueries(promptCallback) {
+  this.viewAllDepartments = function () {
+    database.query('SELECT id, department_name AS name FROM departments', function (err, results) {
+      if (err) throw err;
+      console.table(results);
+      promptCallback(); // Call the promptCallback after displaying the results
+    });
+  };
+
+  this.viewAllRoles = function () {
+    database.query('SELECT roles.id, roles.title, departments.department_name AS department, roles.salary FROM roles JOIN departments ON roles.department_id = departments.id ORDER BY CASE WHEN departments.department_name = "Sales" THEN 0 ELSE 1 END, departments.department_name, roles.id', function (err, results) {
+      if (err) throw err;
+      console.table(results);
+      promptCallback();
+    });
+  };
+
+  this.viewAllEmployees = function () {
+    database.query('SELECT * FROM employees', function (err, results) {
+      if (err) throw err;
+      console.table(results);
+      promptCallback();
+    });
+  };
+
+  this.addDepartment = function (departmentName) {
+    const query = 'INSERT INTO departments (name) VALUES (?)';
+    database.query(query, [departmentName], function (err, results) {
+      if (err) throw err;
+      console.table(results);
+      promptCallback();
+    });
+  };
+
+  // Implement other functions like addRole, addEmployee, updateEmployeeRole, etc.
+}
+
+module.exports = DatabaseQueries;
